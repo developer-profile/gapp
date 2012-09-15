d3b57f41bd77072427e8a5954a4d7de346a3fb65
molokows <d@moloko.ws>
2012-09-15 21:29:51 +0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gapp::Application.config.secret_token = 'b0485b1316c875759f180544c8770dc469cad58aec451ab37f052f42fec33d094cacd49b1737273392fb9888ef09bd1ec1e3bf290816f0ad0e5201f94aaeebdf'
